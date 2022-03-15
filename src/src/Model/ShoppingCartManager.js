2b5443a999210b20c0fe2d26d869fd9f6d68c715
yianan261 <101501539+yianan261@users.noreply.github.com>
2022-03-15 14:08:19 -0700

    Update ShoppingCartManager.js

--- src/src/Model/ShoppingCartManager.js
+++ src/src/Model/ShoppingCartManager.js
@@ -7,34 +7,7 @@ class ShoppingCartManager {
     return [1, 2, 3];
   }
 
-  // testDB() {
-  //   console.log("testing DB");
-  //   const IndexedDb = minimongo.IndexedDb;
-
-  //   // Create IndexedDb
-  //   let db = new IndexedDb(
-  //     { namespace: "mydb" },
-  //     function () {
-  //       // Add a collection to the database
-  //       db.addCollection("animals", function () {
-  //         let doc = { species: "dog", name: "Bingo" };
-
-  //         // Always use upsert for both inserts and modifies
-  //         db.animals.upsert(doc, function () {
-  //           // Success:
-
-  //           // Query dog (with no query options beyond a selector)
-  //           db.animals.findOne({ species: "dog" }, {}, function (res) {
-  //             console.log("Dog's name is: " + res.name);
-  //           });
-  //         });
-  //       });
-  //     },
-  //     function () {
-  //       alert("some error!");
-  //     }
-  //   );
-  // }
+ 
 
   /**
    * addToShopCart method to get the subtotal
